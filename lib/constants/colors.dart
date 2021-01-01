baba0cd18e6277df4a5da962a2500cc00087017a
minthantkyaw28 <wanderhobby@gmail.com>
2021-01-01 14:39:01 +0000

    fix lint warning

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,3 +5,4 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
+// chore: note 2021-01-01T14:39:01
