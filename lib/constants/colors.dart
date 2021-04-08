e514fc628cd00e5c43b30d526c80c633b3a9a427
minthantkyaw28 <wanderhobby@gmail.com>
2021-04-08 17:15:02 +0100

    remove dead code

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,4 +5,3 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
-// chore: note 2021-01-01T14:39:01
