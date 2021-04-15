3ffe9aa92a370031b64fc7cfef013ab913402fc3
minthantkyaw28 <wanderhobby@gmail.com>
2021-04-15 17:01:59 +0100

    trim whitespace

--- lib/network/response/patient_profile_response/patient_profile_info_response.dart
+++ lib/network/response/patient_profile_response/patient_profile_info_response.dart
@@ -80,3 +80,4 @@ class PatientProfileInfoResponse {
   factory PatientProfileInfoResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientProfileInfoResponseFromJson(json);
 }
+// chore: note 2021-04-15T17:01:59
