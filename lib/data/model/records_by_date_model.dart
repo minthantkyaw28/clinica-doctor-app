6004f1d5ab8ca4a3598691eb65988c631914938b
minthantkyaw28 <wanderhobby@gmail.com>
2021-05-21 19:51:23 +0100

    feat: tiny addition

--- lib/data/model/records_by_date_model.dart
+++ lib/data/model/records_by_date_model.dart
@@ -12,3 +12,4 @@ class RecordsByDateModel {
   Future<List<SingleMedicalRecordVO>> getAllMedicalRecordsByDate() =>
       _recordsByDateDateAgent.getRecordsByDate();
 }
+// chore: note 2021-05-21T19:51:23
