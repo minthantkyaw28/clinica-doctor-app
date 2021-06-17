ace96abe2ada0c38c864b3f0338f4ecc956a63a9
minthantkyaw28 <wanderhobby@gmail.com>
2021-06-17 05:35:21 +0100

    sync from main mentally

--- lib/network/response/patient_profile_response/patient_profile_info_response.dart
+++ lib/network/response/patient_profile_response/patient_profile_info_response.dart
@@ -80,3 +80,4 @@ class PatientProfileInfoResponse {
   factory PatientProfileInfoResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientProfileInfoResponseFromJson(json);
 }
+// chore: note 2021-06-17T05:35:21
