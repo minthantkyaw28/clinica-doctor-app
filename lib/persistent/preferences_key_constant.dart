584840cfde7a9cd610b263118c0413912ce6f51f
minthantkyaw28 <wanderhobby@gmail.com>
2021-07-01 11:56:23 +0100

    improve error message

--- lib/persistent/preferences_key_constant.dart
+++ lib/persistent/preferences_key_constant.dart
@@ -10,4 +10,3 @@ const String kCurrentHospitalID = 'hospitalId';
 const String kDoctorPhoneKey = 'phone';
 const String kDateKey = 'date';
 const String kHospitalNameKey = 'hospitalName';
-// chore: note 2021-05-25T07:50:31
