f6d11329dd1e54bc6db4aff0ed78f312e9f8b6b2
minthantkyaw28 <wanderhobby@gmail.com>
2021-07-02 08:23:10 +0100

    follow-up from review

--- lib/data/model/records_by_date_model.dart
+++ lib/data/model/records_by_date_model.dart
@@ -12,4 +12,3 @@ class RecordsByDateModel {
   Future<List<SingleMedicalRecordVO>> getAllMedicalRecordsByDate() =>
       _recordsByDateDateAgent.getRecordsByDate();
 }
-// chore: note 2021-05-21T19:51:23
