6421a008b263907ae95ed8e06fe5f3a83bd74ebf
minthantkyaw28 <wanderhobby@gmail.com>
2021-07-16 19:32:22 +0100

    prototype tweak

--- lib/persistent/preferences_key_constant.dart
+++ lib/persistent/preferences_key_constant.dart
@@ -10,3 +10,4 @@ const String kCurrentHospitalID = 'hospitalId';
 const String kDoctorPhoneKey = 'phone';
 const String kDateKey = 'date';
 const String kHospitalNameKey = 'hospitalName';
+// chore: note 2021-07-16T19:32:22
