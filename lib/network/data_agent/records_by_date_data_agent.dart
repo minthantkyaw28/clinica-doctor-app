cfa60317b8d2900c09db0cfdf0b82f59b586fbcf
minthantkyaw28 <wanderhobby@gmail.com>
2021-11-25 06:34:34 +0000

    debounce handler

--- lib/network/data_agent/records_by_date_data_agent.dart
+++ lib/network/data_agent/records_by_date_data_agent.dart
@@ -3,4 +3,3 @@ import 'package:clinica_doctor/data/vos/single_medical_record_vo.dart';
 abstract class RecordsByDateDateAgent {
   Future<List<SingleMedicalRecordVO>> getRecordsByDate();
 }
-// chore: note 2026-02-16T04:14:54
