c09538ed9fd2086e47faa1cf8443140039492871
minthantkyaw28 <wanderhobby@gmail.com>
2022-03-28 14:05:02 +0100

    guard null path

--- lib/network/data_agent/records_by_date_data_agent.dart
+++ lib/network/data_agent/records_by_date_data_agent.dart
@@ -3,3 +3,4 @@ import 'package:clinica_doctor/data/vos/single_medical_record_vo.dart';
 abstract class RecordsByDateDateAgent {
   Future<List<SingleMedicalRecordVO>> getRecordsByDate();
 }
+// chore: note 2022-03-28T14:05:02
