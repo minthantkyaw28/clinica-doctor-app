0d28da3a46c908523f1052e1b13671d1061f0634
minthantkyaw28 <wanderhobby@gmail.com>
2022-04-14 02:14:55 +0100

    trim whitespace

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -18,4 +18,3 @@ const kPatientNameText = "Name   ";
 const kPatientAgeText = "Age   ";
 const kViewDetailText = "View Details";
 const kPatientRecordText = "Patient Records";
-// chore: note 2022-04-13T19:30:02
