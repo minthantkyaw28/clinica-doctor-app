bfbebd6a2cf040950bf0c9224228cebee6d3b8d2
minthantkyaw28 <wanderhobby@gmail.com>
2022-05-13 19:58:42 +0100

    tweak config

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -18,3 +18,4 @@ const kPatientNameText = "Name   ";
 const kPatientAgeText = "Age   ";
 const kViewDetailText = "View Details";
 const kPatientRecordText = "Patient Records";
+// chore: note 2022-05-13T19:58:42
