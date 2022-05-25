c8357f6ca4f2fad956574b2f6f2f48aafe46a9f0
minthantkyaw28 <wanderhobby@gmail.com>
2022-05-25 16:05:43 +0100

    sync constants

--- lib/network/data_agent/records_by_date_data_agent.dart
+++ lib/network/data_agent/records_by_date_data_agent.dart
@@ -3,3 +3,4 @@ import 'package:clinica_doctor/data/vos/single_medical_record_vo.dart';
 abstract class RecordsByDateDateAgent {
   Future<List<SingleMedicalRecordVO>> getRecordsByDate();
 }
+// chore: note 2022-05-25T16:05:43
