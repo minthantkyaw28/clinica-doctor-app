72e6b517abcfa37300e66be731b78774bff33dfd
minthantkyaw28 <wanderhobby@gmail.com>
2022-08-16 19:11:04 +0100

    clarify comment

--- lib/persistent/preferences_key_constant.dart
+++ lib/persistent/preferences_key_constant.dart
@@ -10,4 +10,3 @@ const String kCurrentHospitalID = 'hospitalId';
 const String kDoctorPhoneKey = 'phone';
 const String kDateKey = 'date';
 const String kHospitalNameKey = 'hospitalName';
-// chore: note 2022-01-20T02:36:22
