76e98c284614b3733f90df9901130dc5dcb905cd
minthantkyaw28 <wanderhobby@gmail.com>
2022-09-12 17:54:24 +0100

    align naming

--- lib/network/api/records_search_by_date_api.dart
+++ lib/network/api/records_search_by_date_api.dart
@@ -15,4 +15,3 @@ abstract class MedicalRecordsByDateAPI {
       @Field(kBodyKeyForDoctor) String doctorId,
       @Field(kBodyKeyForHospital) String hospitalId);
 }
-// chore: note 2022-03-28T19:44:50
