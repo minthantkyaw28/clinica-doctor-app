47f3137d29188cad6f9b7b81436461479c8f91cb
minthantkyaw28 <wanderhobby@gmail.com>
2022-10-04 09:33:27 +0100

    polish docs

--- lib/data/vos/medical_record_vo.g.dart
+++ lib/data/vos/medical_record_vo.g.dart
@@ -43,4 +43,3 @@ Map<String, dynamic> _$MedicalRecordVOToJson(MedicalRecordVO instance) =>
       'doctor_name': instance.doctorName,
       'hospital_clinic_name': instance.hospitalName,
     };
-// chore: note 2022-05-06T11:16:46
