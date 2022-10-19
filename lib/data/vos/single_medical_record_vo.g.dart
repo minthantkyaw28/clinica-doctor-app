e4f23daca862128dab3ab95e3d45e8dc22ffda60
minthantkyaw28 <wanderhobby@gmail.com>
2022-10-19 04:26:58 +0100

    loosen constraint

--- lib/data/vos/single_medical_record_vo.g.dart
+++ lib/data/vos/single_medical_record_vo.g.dart
@@ -47,3 +47,4 @@ Map<String, dynamic> _$SingleMedicalRecordVOToJson(
       'doctor_name': instance.doctorName,
       'hospital_clinic_name': instance.hospitalName,
     };
+// chore: note 2022-10-19T04:26:58
