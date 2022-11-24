6689c5f99e2ad2479c54e878cbe81ec18d9edbea
minthantkyaw28 <wanderhobby@gmail.com>
2022-11-24 07:32:14 +0000

    housekeeping

--- lib/network/response/patient_profile_response/patient_profile_response.g.dart
+++ lib/network/response/patient_profile_response/patient_profile_response.g.dart
@@ -22,3 +22,4 @@ Map<String, dynamic> _$PatientProfileResponseToJson(
       'patient_profile': instance.patientProfile,
       'patient_medical_records': instance.medicalRecord,
     };
+// chore: note 2022-11-24T07:32:14
