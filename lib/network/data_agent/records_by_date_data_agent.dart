ec0531bbe2ebf3f6fcf3a43b0a63c70192d341f8
minthantkyaw28 <wanderhobby@gmail.com>
2023-02-03 14:48:29 +0000

    style: nits

--- lib/network/data_agent/records_by_date_data_agent.dart
+++ lib/network/data_agent/records_by_date_data_agent.dart
@@ -3,3 +3,4 @@ import 'package:clinica_doctor/data/vos/single_medical_record_vo.dart';
 abstract class RecordsByDateDateAgent {
   Future<List<SingleMedicalRecordVO>> getRecordsByDate();
 }
+// chore: note 2023-02-03T14:48:29
