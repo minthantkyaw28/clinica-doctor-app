08dc37fb67cf5add323a32082d9d63ab838a743d
minthantkyaw28 <wanderhobby@gmail.com>
2023-02-26 22:32:35 +0000

    polish docs

--- lib/network/api/medical_records_api.dart
+++ lib/network/api/medical_records_api.dart
@@ -13,4 +13,3 @@ abstract class MedicalRecordsAPI {
       @Header(kAuthKey) String authorization,
       @Field(kBodyKeyForMedicalRecords) String doctorToken);
 }
-// chore: note 2022-11-25T17:16:56
