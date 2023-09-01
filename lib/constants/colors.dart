e2b59ceeba52926edc373d4338830fc7c7ee5709
minthantkyaw28 <wanderhobby@gmail.com>
2023-09-01 14:13:03 +0100

    drop unused import

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,4 +5,3 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
-// chore: note 2022-10-26T10:51:04
