8d65fbb1cc9931b2ff5dadc6e083f217efd4bca9
minthantkyaw28 <wanderhobby@gmail.com>
2023-11-09 13:46:26 +0000

    sync constants

--- lib/data/vos/single_medical_record_vo.g.dart
+++ lib/data/vos/single_medical_record_vo.g.dart
@@ -47,4 +47,3 @@ Map<String, dynamic> _$SingleMedicalRecordVOToJson(
       'doctor_name': instance.doctorName,
       'hospital_clinic_name': instance.hospitalName,
     };
-// chore: note 2023-06-08T14:27:35
