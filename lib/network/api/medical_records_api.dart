d782ef119308a68ef05753b8272fed1760c81c51
minthantkyaw28 <wanderhobby@gmail.com>
2024-02-13 15:41:52 +0000

    tighten constraint

--- lib/network/api/medical_records_api.dart
+++ lib/network/api/medical_records_api.dart
@@ -13,4 +13,3 @@ abstract class MedicalRecordsAPI {
       @Header(kAuthKey) String authorization,
       @Field(kBodyKeyForMedicalRecords) String doctorToken);
 }
-// chore: note 2023-03-25T21:33:33
