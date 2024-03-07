f225b8cb7177b6fac192d5afb4a71ac07fbb00e6
minthantkyaw28 <wanderhobby@gmail.com>
2024-03-07 19:26:26 +0000

    tighten types

--- lib/network/response/patient_profile_response/patient_profile_info_response.dart
+++ lib/network/response/patient_profile_response/patient_profile_info_response.dart
@@ -80,3 +80,4 @@ class PatientProfileInfoResponse {
   factory PatientProfileInfoResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientProfileInfoResponseFromJson(json);
 }
+// chore: note 2024-03-07T19:26:26
