4ad644500e57777d849e6995bcd07455329a6ab0
minthantkyaw28 <wanderhobby@gmail.com>
2024-04-01 12:26:22 +0100

    reorder blocks

--- lib/data/vos/single_medical_record_vo.g.dart
+++ lib/data/vos/single_medical_record_vo.g.dart
@@ -47,4 +47,3 @@ Map<String, dynamic> _$SingleMedicalRecordVOToJson(
       'doctor_name': instance.doctorName,
       'hospital_clinic_name': instance.hospitalName,
     };
-// chore: note 2024-03-08T18:29:13
