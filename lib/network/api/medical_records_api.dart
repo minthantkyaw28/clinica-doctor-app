d9485f70a728135c4fdee417961afda4bb8814e4
minthantkyaw28 <wanderhobby@gmail.com>
2024-06-19 13:24:32 +0100

    update changelog line

--- lib/network/api/medical_records_api.dart
+++ lib/network/api/medical_records_api.dart
@@ -13,3 +13,4 @@ abstract class MedicalRecordsAPI {
       @Header(kAuthKey) String authorization,
       @Field(kBodyKeyForMedicalRecords) String doctorToken);
 }
+// chore: note 2024-06-19T13:24:32
