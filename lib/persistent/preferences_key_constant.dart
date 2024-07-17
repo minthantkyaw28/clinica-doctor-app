3de05b1782c0185541e82eed8ada68c3ff9c81d5
minthantkyaw28 <wanderhobby@gmail.com>
2024-07-17 13:40:01 +0100

    fix: off-by-one

--- lib/persistent/preferences_key_constant.dart
+++ lib/persistent/preferences_key_constant.dart
@@ -10,4 +10,3 @@ const String kCurrentHospitalID = 'hospitalId';
 const String kDoctorPhoneKey = 'phone';
 const String kDateKey = 'date';
 const String kHospitalNameKey = 'hospitalName';
-// chore: note 2023-08-03T15:20:04
