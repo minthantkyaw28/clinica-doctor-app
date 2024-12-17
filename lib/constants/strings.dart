81269e6aada3a66712d4b36f33425af623c3be34
minthantkyaw28 <wanderhobby@gmail.com>
2024-12-17 18:00:10 +0000

    harden validation

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -18,4 +18,3 @@ const kPatientNameText = "Name   ";
 const kPatientAgeText = "Age   ";
 const kViewDetailText = "View Details";
 const kPatientRecordText = "Patient Records";
-// chore: note 2024-12-04T18:02:59
