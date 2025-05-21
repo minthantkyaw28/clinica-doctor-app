fac5e329f6086eedae63e3f98c089b9118367b46
minthantkyaw28 <wanderhobby@gmail.com>
2025-05-21 21:04:36 +0100

    follow-up from review

--- lib/network/api/records_search_by_date_api.dart
+++ lib/network/api/records_search_by_date_api.dart
@@ -15,3 +15,4 @@ abstract class MedicalRecordsByDateAPI {
       @Field(kBodyKeyForDoctor) String doctorId,
       @Field(kBodyKeyForHospital) String hospitalId);
 }
+// chore: note 2025-05-21T21:04:36
