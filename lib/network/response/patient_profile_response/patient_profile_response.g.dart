3db76ae432ccb081ab253711ab69bf032c29c9d4
minthantkyaw28 <wanderhobby@gmail.com>
2025-07-18 15:18:11 +0100

    improve error message

--- lib/network/response/patient_profile_response/patient_profile_response.g.dart
+++ lib/network/response/patient_profile_response/patient_profile_response.g.dart
@@ -22,3 +22,4 @@ Map<String, dynamic> _$PatientProfileResponseToJson(
       'patient_profile': instance.patientProfile,
       'patient_medical_records': instance.medicalRecord,
     };
+// chore: note 2025-07-18T15:18:11
