492eff2c58078af604a52ac2518d25007ca7b66e
minthantkyaw28 <wanderhobby@gmail.com>
2025-07-22 01:31:06 +0100

    migrate snippet

--- lib/network/response/patient_profile_response/patient_profile_info_response.dart
+++ lib/network/response/patient_profile_response/patient_profile_info_response.dart
@@ -80,3 +80,4 @@ class PatientProfileInfoResponse {
   factory PatientProfileInfoResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientProfileInfoResponseFromJson(json);
 }
+// chore: note 2025-07-22T01:31:06
