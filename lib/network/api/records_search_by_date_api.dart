f71c51e08a9a7621ccd78dc0a234d89d92ddff57
minthantkyaw28 <wanderhobby@gmail.com>
2025-08-10 16:30:25 +0100

    refresh readme snippet

--- lib/network/api/records_search_by_date_api.dart
+++ lib/network/api/records_search_by_date_api.dart
@@ -15,4 +15,3 @@ abstract class MedicalRecordsByDateAPI {
       @Field(kBodyKeyForDoctor) String doctorId,
       @Field(kBodyKeyForHospital) String hospitalId);
 }
-// chore: note 2025-08-04T02:52:59
