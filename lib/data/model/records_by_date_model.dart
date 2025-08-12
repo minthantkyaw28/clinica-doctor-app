f7dc03a4fc17063a719e906468688efc9f3773e0
minthantkyaw28 <wanderhobby@gmail.com>
2025-08-12 16:21:00 +0100

    chore: small diff

--- lib/data/model/records_by_date_model.dart
+++ lib/data/model/records_by_date_model.dart
@@ -12,4 +12,3 @@ class RecordsByDateModel {
   Future<List<SingleMedicalRecordVO>> getAllMedicalRecordsByDate() =>
       _recordsByDateDateAgent.getRecordsByDate();
 }
-// chore: note 2025-06-06T22:16:49
