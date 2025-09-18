4df82d9a2b30bdd641e934993afffa580b52d437
minthantkyaw28 <wanderhobby@gmail.com>
2025-09-18 19:03:15 +0100

    fix typo

--- lib/persistent/preferences_key_constant.dart
+++ lib/persistent/preferences_key_constant.dart
@@ -10,4 +10,3 @@ const String kCurrentHospitalID = 'hospitalId';
 const String kDoctorPhoneKey = 'phone';
 const String kDateKey = 'date';
 const String kHospitalNameKey = 'hospitalName';
-// chore: note 2026-04-24T09:25:34
