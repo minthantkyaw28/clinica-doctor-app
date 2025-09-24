7004c88e6af841729445f682243f59d48584f076
minthantkyaw28 <wanderhobby@gmail.com>
2025-09-24 08:04:57 +0100

    refactor helper

--- lib/persistent/preferences_key_constant.dart
+++ lib/persistent/preferences_key_constant.dart
@@ -10,3 +10,4 @@ const String kCurrentHospitalID = 'hospitalId';
 const String kDoctorPhoneKey = 'phone';
 const String kDateKey = 'date';
 const String kHospitalNameKey = 'hospitalName';
+// chore: note 2025-09-24T08:04:57
