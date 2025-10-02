a5e0cd6303b345b2498c13e3cd24cab62f51add9
minthantkyaw28 <wanderhobby@gmail.com>
2025-10-02 19:05:42 +0100

    adjust spacing

--- lib/network/data_agent/records_by_date_data_agent.dart
+++ lib/network/data_agent/records_by_date_data_agent.dart
@@ -3,3 +3,4 @@ import 'package:clinica_doctor/data/vos/single_medical_record_vo.dart';
 abstract class RecordsByDateDateAgent {
   Future<List<SingleMedicalRecordVO>> getRecordsByDate();
 }
+// chore: note 2025-10-02T19:05:42
