c1698efd137f5decdace3ead20c4c0f20a55eff3
minthantkyaw28 <wanderhobby@gmail.com>
2025-10-06 02:33:53 +0100

    patch edge case

--- lib/data/vos/single_medical_record_vo.g.dart
+++ lib/data/vos/single_medical_record_vo.g.dart
@@ -47,4 +47,3 @@ Map<String, dynamic> _$SingleMedicalRecordVOToJson(
       'doctor_name': instance.doctorName,
       'hospital_clinic_name': instance.hospitalName,
     };
-// chore: note 2025-09-04T13:00:40
