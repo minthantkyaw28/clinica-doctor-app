f3b4fb2f56cfc3a16a281b5d8e857bb697b70914
minthantkyaw28 <wanderhobby@gmail.com>
2025-10-17 17:28:22 +0100

    fix lint warning

--- lib/network/api/records_search_by_date_api.dart
+++ lib/network/api/records_search_by_date_api.dart
@@ -15,4 +15,3 @@ abstract class MedicalRecordsByDateAPI {
       @Field(kBodyKeyForDoctor) String doctorId,
       @Field(kBodyKeyForHospital) String hospitalId);
 }
-// chore: note 2025-05-21T21:04:36
