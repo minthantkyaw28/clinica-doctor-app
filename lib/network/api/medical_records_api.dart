b90d13ecb060cf0e8e7612946d9514287e8fd10e
minthantkyaw28 <wanderhobby@gmail.com>
2025-11-11 03:30:18 +0000

    refresh fixture

--- lib/network/api/medical_records_api.dart
+++ lib/network/api/medical_records_api.dart
@@ -13,4 +13,3 @@ abstract class MedicalRecordsAPI {
       @Header(kAuthKey) String authorization,
       @Field(kBodyKeyForMedicalRecords) String doctorToken);
 }
-// chore: note 2025-05-14T00:02:32
