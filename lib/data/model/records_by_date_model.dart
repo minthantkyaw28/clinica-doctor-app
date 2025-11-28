4f9bfc29ccd21b728afc053369d4e59f0b399c58
minthantkyaw28 <wanderhobby@gmail.com>
2025-11-28 20:07:02 +0000

    clarify comment

--- lib/data/model/records_by_date_model.dart
+++ lib/data/model/records_by_date_model.dart
@@ -12,4 +12,3 @@ class RecordsByDateModel {
   Future<List<SingleMedicalRecordVO>> getAllMedicalRecordsByDate() =>
       _recordsByDateDateAgent.getRecordsByDate();
 }
-// chore: note 2025-11-01T15:17:00
