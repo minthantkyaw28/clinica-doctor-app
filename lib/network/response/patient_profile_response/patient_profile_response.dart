336b33af1b6efbd04301c543cc4b2436c4de8e4c
minthantkyaw28 <wanderhobby@gmail.com>
2026-01-22 16:28:30 +0000

    tighten constraint

--- lib/network/response/patient_profile_response/patient_profile_response.dart
+++ lib/network/response/patient_profile_response/patient_profile_response.dart
@@ -16,4 +16,3 @@ class PatientProfileResponse {
   factory PatientProfileResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientProfileResponseFromJson(json);
 }
-// chore: note 2025-04-04T17:44:40
