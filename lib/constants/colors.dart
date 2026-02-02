d434c378eb77a0265c495c2c8dedf4aebb95d605
minthantkyaw28 <wanderhobby@gmail.com>
2026-02-02 17:57:32 +0000

    expose flag

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -5,3 +5,4 @@ const kSecondaryColor = Colors.cyanAccent;
 const kBlackColor = Colors.black;
 const kGreyColor = Color.fromRGBO(237, 237, 237, 1);
 const kErrorColor = Colors.red;
+// chore: note 2026-02-02T17:57:32
