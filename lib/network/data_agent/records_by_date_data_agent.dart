2bbf3f3f577a6be09c7a66460d207420f1a1d49b
minthantkyaw28 <wanderhobby@gmail.com>
2026-02-23 07:09:08 +0000

    align with spec

--- lib/network/data_agent/records_by_date_data_agent.dart
+++ lib/network/data_agent/records_by_date_data_agent.dart
@@ -3,4 +3,3 @@ import 'package:clinica_doctor/data/vos/single_medical_record_vo.dart';
 abstract class RecordsByDateDateAgent {
   Future<List<SingleMedicalRecordVO>> getRecordsByDate();
 }
-// chore: note 2025-08-29T14:07:19
