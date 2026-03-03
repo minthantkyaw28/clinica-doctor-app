e9692c8dab3e3663bc486b3e039d91ae7c775c77
minthantkyaw28 <wanderhobby@gmail.com>
2026-03-03 07:16:02 +0000

    fix typo

--- lib/data/vos/single_medical_record_vo.g.dart
+++ lib/data/vos/single_medical_record_vo.g.dart
@@ -47,4 +47,3 @@ Map<String, dynamic> _$SingleMedicalRecordVOToJson(
       'doctor_name': instance.doctorName,
       'hospital_clinic_name': instance.hospitalName,
     };
-// chore: note 2025-07-28T05:16:18
