955f4676164b51327b6a555f0096d586b7329741
minthantkyaw28 <wanderhobby@gmail.com>
2026-03-19 23:28:50 +0000

    update changelog line

--- lib/network/response/patient_profile_response/patient_profile_response.dart
+++ lib/network/response/patient_profile_response/patient_profile_response.dart
@@ -16,3 +16,4 @@ class PatientProfileResponse {
   factory PatientProfileResponse.fromJson(Map<String, dynamic> json) =>
       _$PatientProfileResponseFromJson(json);
 }
+// chore: note 2026-03-19T23:28:50
