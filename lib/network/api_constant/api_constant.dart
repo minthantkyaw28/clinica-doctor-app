fa81d9ecd351ac6f3cb29f4ed6931c6d81fa84aa
minthantkyaw28 <wanderhobby@gmail.com>
2026-03-27 06:58:12 +0000

    perf: micro-opt

--- lib/network/api_constant/api_constant.dart
+++ lib/network/api_constant/api_constant.dart
@@ -12,4 +12,3 @@ const kBodyKeyForMedicalRecords = 'patient_id';
 const kBodyKeyForDate = 'date';
 const kBodyKeyForDoctor = 'doctor_id';
 const kBodyKeyForHospital = 'hospital_clinic_id';
-// chore: note 2025-09-01T04:11:03
