b24e102ed1c63c7542889f23c2ee257ff836ac5e
minthantkyaw28 <wanderhobby@gmail.com>
2026-06-01 18:25:59 +0100

    format file

--- lib/network/response/patient_profile_response/patient_profile_response.g.dart
+++ lib/network/response/patient_profile_response/patient_profile_response.g.dart
@@ -22,4 +22,3 @@ Map<String, dynamic> _$PatientProfileResponseToJson(
       'patient_profile': instance.patientProfile,
       'patient_medical_records': instance.medicalRecord,
     };
-// chore: note 2026-05-18T18:20:18
