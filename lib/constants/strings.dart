8943f510ffe9be7fabcd464a44db5eb9ff2bedd2
minthantkyaw28 <wanderhobby@gmail.com>
2026-06-02 04:48:38 +0100

    clean up imports

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -18,4 +18,3 @@ const kPatientNameText = "Name   ";
 const kPatientAgeText = "Age   ";
 const kViewDetailText = "View Details";
 const kPatientRecordText = "Patient Records";
-// chore: note 2025-08-22T17:18:28
