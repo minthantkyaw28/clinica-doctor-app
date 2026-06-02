eea0b1f0a95679eb73e5a63dd7979f4002c95212
minthantkyaw28 <wanderhobby@gmail.com>
2026-06-02 10:30:19 +0100

    update notes

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -18,3 +18,4 @@ const kPatientNameText = "Name   ";
 const kPatientAgeText = "Age   ";
 const kViewDetailText = "View Details";
 const kPatientRecordText = "Patient Records";
+// chore: note 2026-06-02T10:30:19
